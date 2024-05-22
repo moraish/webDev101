8b78331e27111d7c0c81c5758590b510a18b38ac
Moraish Kapoor <moraish.kapoor@hotmail.com>
2024-05-23 01:58:47 +0530

    added minor css

--- Quiz/App/src/App.jsx
+++ Quiz/App/src/App.jsx
@@ -5,20 +5,11 @@ import { Landing } from './components/Landing'
 import { Question } from './components/Question'
 import Container from '@mui/material/Container';
 import { QuizGenerator } from './components/QuizGenerator';
+import questions from './data/questions';
 
 function App() {
 
-  const [questionSet, setQuestionSet] = useState([{
-    "id": 1,
-    "question_text": "What is the capital of India?",
-    "answer": "New Delhi",
-    "options_text": ["New Delhi", "Mumbai", "Chennai", "Kolkata", "Shimla"]
-  }, {
-    "id": 2,
-    "question_text": "What is the capital of USA?",
-    "answer": "Washington",
-    "options_text": ["New York", "Washington", "California", "Seattle"]
-  }]);
+  const [questionSet, setQuestionSet] = useState(questions);
 
   return (
     <>
---
+++ Quiz/App/src/data/questions.js
@@ -0,0 +1,13 @@
+const questions = [{
+    "id": 1,
+    "question_text": "What is the capital of India?",
+    "answer": "New Delhi",
+    "options_text": ["New Delhi", "Mumbai", "Chennai", "Kolkata", "Shimla"]
+  }, {
+    "id": 2,
+    "question_text": "What is the capital of USA?",
+    "answer": "Washington",
+    "options_text": ["New York", "Washington", "California", "Seattle"]
+  }]
+
+export default questions;
