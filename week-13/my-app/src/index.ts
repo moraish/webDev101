012c2081002805ad2a7a05a7422a21aa1c4ba843
Moraish Kapoor <moraish.kapoor@hotmail.com>
2025-01-30 18:53:33 -0500

    added jwt

--- week-13/my-app/src/index.ts
+++ week-13/my-app/src/index.ts
@@ -1,10 +1,13 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
+
 
 const app = new Hono<{
   Bindings: {
-    DATABASE_URL: string
+    DATABASE_URL: string,
+    JET_SECRET: string
   }
 }>()
 
@@ -19,8 +22,8 @@ app.post('/api/v1/user/signup', async (c) => {
   // needs to add 1. ZOD
   // 2. Password enctyption
   try {
-    console.log(body)
-    await prisma.user.create({
+
+    const user = await prisma.user.create({
       data: {
         username: body.username,
         password: body.password,
@@ -28,7 +31,11 @@ app.post('/api/v1/user/signup', async (c) => {
         lastName: body.lastName
       }
     })
-    return c.text("User Created!")
+    const jwt = await sign({
+      id: user.id,
+    }, c.env.JET_SECRET);
+
+    return c.text(jwt);
   } catch (e) {
     console.log(e)
     c.status(411);
@@ -36,8 +43,41 @@ app.post('/api/v1/user/signup', async (c) => {
   }
 })
 
-app.post('/api/v1/user/signin', (c) => {
-  return c.text('Hello Hono!')
+
+
+app.post('/api/v1/user/signin', async (c) => {
+  const body = await c.req.json();
+  console.log(body);
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        username: body.username,
+        password: body.password
+      }
+    })
+    if (!user) {
+      c.status(403); // unauthorized
+      return c.json({
+        message: "invalid credentials"
+      })
+    }
+    const jwt = await sign({
+      id: user.id
+    }, c.env.JET_SECRET)
+
+    return c.text(jwt)
+
+
+  } catch (e) {
+    console.log(e)
+    return c.text("error occured");
+  }
+
+
 })
 
 app.post('/api/v1/blog', (c) => {
