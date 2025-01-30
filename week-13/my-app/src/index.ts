da6dd2717d3f4e7f0120f7f7340b473f000a5125
Moraish Kapoor <moraish.kapoor@hotmail.com>
2025-01-30 06:15:53 -0500

    signup route created

--- week-13/my-app/src/index.ts
+++ week-13/my-app/src/index.ts
@@ -8,12 +8,32 @@ const app = new Hono<{
   }
 }>()
 
-app.post('/api/v1/user/signup', (c) => {
+app.post('/api/v1/user/signup', async (c) => {
+
+  const body = await c.req.json();
+
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate())
 
-  return c.text('Hello Hono!')
+  // needs to add 1. ZOD
+  // 2. Password enctyption
+  try {
+    console.log(body)
+    await prisma.user.create({
+      data: {
+        username: body.username,
+        password: body.password,
+        firstName: body.firstName,
+        lastName: body.lastName
+      }
+    })
+    return c.text("User Created!")
+  } catch (e) {
+    console.log(e)
+    c.status(411);
+    return c.text("Invalid username")
+  }
 })
 
 app.post('/api/v1/user/signin', (c) => {
@@ -43,4 +63,6 @@ app.get('/api/v1/blog/bulk', (c) => {
 
 
 
-export default app
+export default app;
+
+// api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiYjExMDE1NWItZjJhOC00NjZkLTg2YjctYjBjMWY5OThkYTQ3IiwidGVuYW50X2lkIjoiZWNmMTFjNWU2NmFkNDcyYzg0ZmY3MmMxMzE2NmZiMzMxYzU4MDRlODM0MDAyZDFiNzEyNjhmMzM2MTZkNjk5YSIsImludGVybmFsX3NlY3JldCI6IjU4ODk3ODczLWRmMWEtNGVhMi04MDk5LTMxNTkxZDA0YjEyMiJ9.fSl6PWfdNnXbmFDWQyTZ7V7-A2PrHkek81EuETjJPd8
