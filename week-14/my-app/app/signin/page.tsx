2c9ac9377ff06e3843733275b6542d106e511740
Moraish Kapoor <moraish.kapoor@hotmail.com>
2025-02-05 05:10:06 -0500

    nextjs basics

---
+++ week-14/my-app/app/signin/page.tsx
@@ -0,0 +1,16 @@
+export default function Signin() {
+    return (
+        <div className="flex justify-center p-5 m-5 border">
+            <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900">Username</label>
+                <input type="text" id="small-input" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+
+
+                <label className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                <input type="text" id="small-input" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+
+                <button type="button" className="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sign in</button>
+            </div>
+        </div>
+    )
+}
